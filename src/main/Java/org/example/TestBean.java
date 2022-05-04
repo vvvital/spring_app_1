07c8e0f00813daf3fd671259b82b316a92b1a62c
vvvital <vvvital>
2022-05-04 16:47:45 +0300

    Initial commit

---
+++ src/main/Java/org/example/TestBean.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String nane) {
+        this.name = nane;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
